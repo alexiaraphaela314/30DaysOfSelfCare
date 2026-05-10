3189afe5bf8ff84d53d31081e1c3f8df34fa34f1
alexiaraphaela314 <alexiaraphaela314@gmail.com>
2026-05-10 11:27:53 -0300

    add correções e estruturação do código(boas práticas)

--- app/src/main/java/com/example/a30daysofselfcare/MainActivity.kt
+++ app/src/main/java/com/example/a30daysofselfcare/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
@@ -18,22 +16,40 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose.DaysOfSelfCareTheme
+import com.example.a30daysofselfcare.data.Care
+import com.example.a30daysofselfcare.data.CareRepository
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DaysOfSelfCareTheme{
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            DaysOfSelfCareTheme {
+                // Chamamos apenas o seu App aqui dentro
+                SelfCareApp()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SelfCareTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "30 Days of Self Care"
+            )
+        },
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -44,7 +60,7 @@ fun SelfCareApp() {
         }
     ) { it ->
         LazyColumn(contentPadding = it) {
-            items(cares) {
+            items(CareRepository.cares) {
                 CareItem(
                     care = it,
                     modifier = Modifier
@@ -60,7 +76,7 @@ fun CareItem(
     care: Care,
     modifier: Modifier = Modifier,
 ) {
-    var expanded by remember { mutableSetOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
     Card(modifier = modifier) {
         Column(
